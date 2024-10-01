9d15dd791a4c010408155cd7c9adc49b22135b43
heyanlu <he.yanl@northeastern.edu>
2024-10-01 12:59:16 -0700

    Added calulator xml

--- app/src/main/java/com/example/a1helloworldapp/MainActivity.java
+++ app/src/main/java/com/example/a1helloworldapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.a1helloworldapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -14,8 +16,20 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        //about me button
         Button aboutMeButton = findViewById(R.id.button);
-
         aboutMeButton.setOnClickListener(new AboutMeButtonClickListener(this));
+
+        //quick cal button
+        Button quickCalButton = findViewById(R.id.button_cal);
+        quickCalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //intent helps to move from the current MainActivity to the new activity
+                Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
