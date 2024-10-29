f7c213875684b92f475c3fd74e054acc8d5d878e
heyanlu <he.yanl@northeastern.edu>
2024-10-28 21:00:53 -0700

    Added adapter

--- app/src/main/java/com/example/a1helloworldapp/adapter/ContactsAdapter.java
+++ app/src/main/java/com/example/a1helloworldapp/adapter/ContactsAdapter.java
@@ -1,5 +1,53 @@
 package com.example.a1helloworldapp.adapter;
 
-public class ContactsAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.a1helloworldapp.R;
+import com.example.a1helloworldapp.model.Contact;
+
+import java.util.List;
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ContactViewHolder> {
+    private List<Contact> contactsList;
+
+    public ContactsAdapter(List<Contact> contactsList) {
+        this.contactsList = contactsList;
+    }
+
+    @NonNull
+    @Override
+    public ContactViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_add_contact, parent, false);
+        return new ContactViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactViewHolder holder, int position) {
+        Contact contact = contactsList.get(position);
+        holder.nameTextView.setText(contact.getName());
+        holder.phoneTextView.setText(contact.getPhoneNumber());
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactsList.size();
+    }
+
+    public static class ContactViewHolder extends RecyclerView.ViewHolder {
+        TextView nameTextView;
+        TextView phoneTextView;
+
+        public ContactViewHolder(View itemView) {
+            super(itemView);
+            nameTextView = itemView.findViewById(R.id.editTextName);
+            phoneTextView = itemView.findViewById(R.id.editTextPhoneNumber);
+        }
+    }
 }
